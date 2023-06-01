6d2b670c6216cb8f920ebdd05b5ac72cdec9eb61
zengguangyu <zengguangyu@graspyun.com>
2023-06-01 18:23:41 +0800

    feat: no env

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -3,10 +3,11 @@ import axios from "axios";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   try {
     const { component_access_token, appid, js_code } = event.queryStringParameters
-    const response = await axios.get(`https://api.weixin.qq.com/sns/component/jscode2session?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=${process.env.WECHAT_COMPONENT_APPID}&component_access_token=${component_access_token}`)
+    const response = await axios.get(`https://api.weixin.qq.com/sns/component/jscode2session?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=
+    wxf4bdd6c7a6f01918&component_access_token=${component_access_token}`)
     return {
       statusCode: 200,
-      body: JSON.stringify({ openid: response.data.openid })
+      body: JSON.stringify(response)
     }
   } catch (err) {
     console.log(err)
