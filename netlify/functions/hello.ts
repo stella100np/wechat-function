ecf615b26bb125dcaa9a047106a2e169392fc1c3
zengguangyu <zengguangyu@graspyun.com>
2023-06-01 17:53:27 +0800

    feat: jscode2session

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,10 +1,20 @@
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-
+import type { Handler, HandlerEvent, HandlerContext, HandlerCallback } from "@netlify/functions";
+import axios from "axios";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
+  try {
+    const { component_access_token, appid, js_code } = event.queryStringParameters
+    const response = await axios.get(`https://api.weixin.qq.com/sns/component/jscode2session?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=${process.env.WECHAT_COMPONENT_APPID}&component_access_token=${component_access_token}`)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ openid: response.data.openid })
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: 'Error' })
+    }
+  }
 };
 
 export { handler };
