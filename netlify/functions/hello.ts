f25bdf18467589b593568fa84a04e77767aebc26
zengguangyu <zengguangyu@graspyun.com>
2023-06-02 10:06:17 +0800

    fix: url error

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -3,9 +3,12 @@ import axios from "axios";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   try {
     const { component_access_token, appid, js_code } = event.queryStringParameters
-    const response = await axios.get(`https://api.weixin.qq.com/sns/component/jscode2session?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=
-    wxf4bdd6c7a6f01918&component_access_token=${component_access_token}`)
-    // console.log(response)
+    // console.log("event.queryStringParameters",event.queryStringParameters)
+    const domain = "https://api.weixin.qq.com/sns/component/jscode2session"
+    const url = `${domain}?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=wxf4bdd6c7a6f01918&component_access_token=${component_access_token}`
+    // console.log("链接：",url)
+    const response = await axios.get(url)
+    // console.log("响应：",response.data)
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)
