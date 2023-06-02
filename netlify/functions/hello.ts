b101160b30b21158951efbbb9610f25bc93975eb
zengguangyu <zengguangyu@graspyun.com>
2023-06-02 09:44:20 +0800

    fix: 展示response

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -5,9 +5,10 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     const { component_access_token, appid, js_code } = event.queryStringParameters
     const response = await axios.get(`https://api.weixin.qq.com/sns/component/jscode2session?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=
     wxf4bdd6c7a6f01918&component_access_token=${component_access_token}`)
+    // console.log(response)
     return {
       statusCode: 200,
-      body: JSON.stringify(response)
+      body: JSON.stringify(response.data)
     }
   } catch (err) {
     console.log(err)
