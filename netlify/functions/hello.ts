67cc37c02c93f48a6299224af157466fe0606df2
zengguangyu <zengguangyu@graspyun.com>
2023-06-08 11:45:48 +0800

    feat: 改为公司的开放平台

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -5,7 +5,7 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     const { component_access_token, appid, js_code } = event.queryStringParameters
     // console.log("event.queryStringParameters",event.queryStringParameters)
     const domain = "https://api.weixin.qq.com/sns/component/jscode2session"
-    const url = `${domain}?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=wxf4bdd6c7a6f01918&component_access_token=${component_access_token}`
+    const url = `${domain}?appid=${appid}&js_code=${js_code}&grant_type=authorization_code&component_appid=wx07f2626557a554a0&component_access_token=${component_access_token}`
     // console.log("链接：",url)
     const response = await axios.get(url)
     // console.log("响应：",response.data)
